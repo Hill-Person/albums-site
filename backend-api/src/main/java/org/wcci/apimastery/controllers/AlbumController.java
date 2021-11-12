0bfbaecc1fca84b65ead20a3820ae97e2c477a50
jgard2019 <jwgardner22@gmail.com>
2021-11-12 13:06:16 -0500

    added small controller features

--- backend-api/src/main/java/org/wcci/apimastery/controllers/AlbumController.java
+++ backend-api/src/main/java/org/wcci/apimastery/controllers/AlbumController.java
@@ -1,9 +1,8 @@
 package org.wcci.apimastery.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.wcci.apimastery.model.Album;
+import org.wcci.apimastery.model.Song;
 import org.wcci.apimastery.repositories.AlbumRepository;
 import org.wcci.apimastery.repositories.SongRepository;
 
@@ -23,4 +22,25 @@ public class AlbumController {
     public Iterable<Album> retrieveAllAlbums(){
         return albumRepo.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Album retrieveSingleAlbum(@PathVariable long id){
+        return albumRepo.findById(id).get();
+    }
+
+
+    @PostMapping("/")
+    public Iterable<Album> addAlbum(@RequestBody Album addAlbum){
+        albumRepo.save(addAlbum);
+        return albumRepo.findAll();
+    }
+
+//    @PostMapping("/")
+//    public Album addSong(@RequestBody Song addSong, @PathVariable long id) {
+//
+//        songRepo.save(addSong);
+//        return albumRepo.findById(id).get();
+//    }
+
+
 }
