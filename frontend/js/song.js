2d43af8fa71a438dbd4c722a1b8ebae6e36ed767
Hill-Person <barkerty@gmail.com>
2021-11-12 13:25:14 -0500

    js structure

---
+++ frontend/js/song.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Song</title>
+//     <link rel="stylesheet" href="/frontend/css/style.css">
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+// </head>
+// <body>
+//     <div class="navbar">
+//         <div class="dropdown">
+//           <button class="dropbtn">Menu
+//             <i class="fa fa-caret-down"></i>
+//           </button>
+//           <div class="dropdown-content">
+//             <a href="#" class="dropdown-content-list">Artists</a>
+//             <a href="#" class="dropdown-content-list">Albums</a>
+//             <a href="#" class="dropdown-content-list">Songs</a>
+//           </div>
+//         </div> 
+//       </div>
+
+
+//     <h1 class="title">Arnold's Dive In Drive In</h1>
+
+//     <div class="tracklist">
+//         <ul class="track-list">
+//             <li class="list">1. War Pigs</li>
+//             <!-- <li class="list">2. Paranoid</li>
+//             <li class="list">3. Planet Caravan</li>
+//             <li class="list">4. Iron Man</li>
+//             <li class="list">5. Electric Funeral</li>
+//             <li class="list">6. Hand Of Doom</li>
+//             <li class="list">7. Rat Salad</li>
+//             <li class="list">8. Fairies Wear Boots</li> -->
+//         </ul>
+
+//     </div>
+
+//     <!-- Add a song to the song list -->
+    
+// </body>
+// </html>
