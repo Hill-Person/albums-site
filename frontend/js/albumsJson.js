0193c08297dd0206f2f4dd87a90e4b3ff18678d1
jaredphillips81 <90348394+jaredphillips81@users.noreply.github.com>
2021-11-17 10:16:56 -0500

    album.js and albumJson

--- frontend/js/albumsJson.js
+++ frontend/js/albumsJson.js
@@ -1,8 +1,11 @@
-const albumsJson = [
+const albumsJson = 
+[
     {
       "id": 1,
       "name": "Paranoid",
       "description": "The Plymouth Rock of heavy rock.",
+      "artist": "Black Sabbath",
+      "imgUrl": null,
       "songs": [
         {
           "id": 2,
@@ -50,6 +53,8 @@ const albumsJson = [
       "id": 10,
       "name": "Philosophy Of The World",
       "description": "The Better Beatles.",
+      "artist": "The Shaggs",
+      "imgUrl": null,
       "songs": [
         {
           "id": 11,
@@ -112,7 +117,149 @@ const albumsJson = [
           "artist": "The Shaggs"
         }
       ]
-    }
-  ]
+    },
+    {
+      "id": 23,
+      "name": "198XAD",
+      "description": "Electronic music",
+      "artist": "Mega Drive",
+      "imgUrl": null,
+      "songs": [
+        {
+          "id": 24,
+          "name": "Infiltrate",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 25,
+          "name": "Acid Spit",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 26,
+          "name": "NARC",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 27,
+          "name": "Memory Dealer",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 28,
+          "name": "Osaka Sewers",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 29,
+          "name": "Exoskeleton",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 30,
+          "name": "Murderlize 'em",
+          "artist": "Mega drive"
+        },
+        {
+          "id": 31,
+          "name": "Haunted(Hunted)",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 32,
+          "name": "Edge of Reality",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 33,
+          "name": "Video Stalker",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 34,
+          "name": "The Reducer",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 35,
+          "name": "Slum Lord",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 36,
+          "name": "Zero Point Non-Response",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 37,
+          "name": "Only One",
+          "artist": "Mega Drive"
+        },
+        {
+          "id": 42,
+          "name": "Lookin' For Love",
+          "artist": "Neil Young"
+        }
+      ]
+    },
+    {
+      "id": 38,
+      "name": "Zuma",
+      "description": "Classic Rock",
+      "artist": "Neil Young",
+      "imgUrl": null,
+      "songs": [
+        {
+          "id": 39,
+          "name": "Don't Cry No Tears",
+          "artist": "Neil Young"
+        },
+        {
+          "id": 40,
+          "name": "Danger Bird",
+          "artist": "Neil Young"
+        },
+        {
+          "id": 41,
+          "name": "Pardon My Heart",
+          "artist": "Neil Young"
+        },
+        {
+          "id": 43,
+          "name": "Osaka Sewers",
+          "artist": "Neil Young"
+        },
+        {
+          "id": 44,
+          "name": "Exoskeleton",
+          "artist": "Neil Young"
+        },
+        {
+          "id": 45,
+          "name": "Murderlize 'em",
+          "artist": "Neil Young"
+        },
+        {
+          "id": 46,
+          "name": "Haunted(Hunted)",
+          "artist": "Neil Young"
+        },
+        {
+          "id": 47,
+          "name": "Edge of Reality",
+          "artist": "Neil Young"
+        }
+      ]
+]
+//    {
+//      "id": 48,
+//      "name": "A NfdsdfdsfdsEW ALBUM",
+//      "description": "DESCRIPTION OF NEW ALBUM",
+//      "artist": null,
+//      "imgUrl": null,
+//     "songs": []
+//    }
+//]
 
-  export {albumsJson};
+
+  
